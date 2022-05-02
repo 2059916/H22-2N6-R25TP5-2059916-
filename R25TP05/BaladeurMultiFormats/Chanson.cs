adae169c57983f91db56974cfe5632f7a617ea20
Francois <2059916@cegepmontpetit.ca>
2022-05-02 08:52:50 -0400

    IChanson

---
+++ R25TP05/BaladeurMultiFormats/Chanson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaladeurMultiFormats
+{
+    class Chanson : IChanson
+    {
+        private int m_annee;
+        private string m_artiste;
+        private string m_nomFichier;
+        private string m_titre;
+
+        public Chanson(string pNomFichier)
+        {
+            m_nomFichier = pNomFichier;
+        }
+
+        public Chanson(string pRepertoire, string pArtiste, string pTitre, int pAnnee)
+        {
+
+        }
+
+    }
+}
---
+++ R25TP05/BaladeurMultiFormats/IChanson.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BaladeurMultiFormats
+{
+    class IChanson
+    {
+        public int Annee { get; }
+        public string Artiste { get; }
+        public string Format { get; }
+        public string NomFichier { get; }
+        public string Paroles { get; }
+        public string Titre { get; }
+
+        public void LireEntete()
+        {
+
+        }
+
+        public void LireParoles(StreamReader pobjFichier)
+        {
+
+        }
+        public void SauterEntete(StreamReader pobjFichier)
+        {
+
+        }
+        public void EcrireEntete(StreamWriter pobjFichier)
+        {
+
+        }
+        public void EcrireParoles(StreamWriter pobjFichier, string pParoles)
+        {
+
+        }
+        public void Ecrire(string pParoles)
+        {
+
+        }
+    }
+}

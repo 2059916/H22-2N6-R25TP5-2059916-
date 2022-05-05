826c5f3f20cb061874e0493b4d02002a54caa878
2059916 <2059916@cegepmontpetit.ca>
2022-05-05 10:35:39 -0400

    Test unitaires

--- R25TP05/BaladeurMultiFormatsTests/UnitTestConsultationTODOs.cs
+++ R25TP05/BaladeurMultiFormatsTests/UnitTestConsultationTODOs.cs
@@ -31,68 +31,86 @@ namespace BaladeurMultiFormats.Tests
         // Compléter la méthode pour tester le constructeur au cas où la chanson passée en 
         // deuxième paramètre est à null
 
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void ConsultationTestPourUneConsulationParam2NullTest()
         {
 
             // Arrange/Act : Instancier un objet Consultation avec la date actuelle et une chanson à null
             // À compléter...
+            Consultation consultation_Test = new Consultation(DateTime.Now, null);
 
-           
             // Assert : Vérifier si le constructeur lève une exception ArgumentNullException
             // À compléter...
+
+            //[ExpectedException(typeof(ArgumentNullException))]
         }
 
         // TODO Test B : ConsultationTestParamètreDateTest
         // Compléter la méthode pour tester si la propriété Date de la classe Consultation
-        
-       
+
+        [TestMethod()]
         public void ConsultationTestParamètreDateTest()
         {
             // Arrange : Instancier un objet ChansonsAAC 
             // Instancier un objet DateTime pour la date actuelle
             // Instancier un objet consultation en utilisant les deux objets que vous venez de créer
             // À compléter...
+            ChansonAAC chanson_Test = new ChansonAAC("Chansons\\Blame It On Me.aac");
+            DateTime date_Test = DateTime.Now;
 
+            Consultation consultation_Test = new Consultation(date_Test, chanson_Test);
             // Act : Récupérer la date de consultation de la chanson en utilisant la propriété Date 
             // À compléter...
+            DateTime date = consultation_Test.Date;
 
             // Assert : Vérifier si la propriété Date retourne la bonne date
             // À compléter...
-
+            Assert.AreEqual(date_Test, date);
 
         }
 
         // TODO Test C : ConsultationTestParamètreDélaiTest
         // Compléter la méthode pour tester la propriété Délai
+        [TestMethod()]
         public void ConsultationTestParamètreDélaiTest()
         {
             // Arrange : Instancier un objet ChansonsAAC 
             // Instancier un objet DateTime pour le 1er janvier 2021
             // Instancier un objet consultation en utilisant les deux objets que vous venez de créer
             // À compléter...
+            ChansonAAC chanson_Test = new ChansonAAC("Chansons\\Blame It On Me.aac");
+            DateTime date_Test = new DateTime(2021, 1, 1);
 
+            Consultation consultation_Test = new Consultation(date_Test, chanson_Test);
             // Act : Récupérer le délai de la en utilisant la propriété Délai
             // À compléter...
-
+            int delai = consultation_Test.Délai;
 
             // Assert : Vérifier si la propriété Délai calcule et retourne le bon délai 
             // À compléter...
+            Assert.AreEqual((int)((DateTime.Now - date_Test).TotalSeconds), delai);
 
         }
         // TODO Test D : ConsultationTestParamètreChansonTest
         // Compléter la méthode pour tester la propriété LaChanson
+        [TestMethod()]
         public void ConsultationTestParamètreChansonTest()
         {
             // Arrange : Instancier un objet ChansonsAAC 
             // Instancier un objet consultation avec la date actuelle et l'objet ChansonAAC
             // À compléter...
+            ChansonAAC chanson_Test = new ChansonAAC("Chansons\\Blame It On Me.aac");
+
+            Consultation consultation_Test = new Consultation(DateTime.Now, chanson_Test);
 
             // Act : Récupérer la chanson avec la propriété LaChanson
             // À compléter...
-
+            object chanson = consultation_Test.LaChanson;
 
             // Assert : Vérifier si la propriété LaChanson retourne la bonne chanson
             // À compléter...
+            Assert.AreEqual(chanson_Test, chanson);
 
         }
     }
--- R25TP05/BaladeurMultiFormatsTests/UnitTestHistoriqueTODOs.cs
+++ R25TP05/BaladeurMultiFormatsTests/UnitTestHistoriqueTODOs.cs
@@ -11,22 +11,27 @@ namespace BaladeurMultiFormatsTests
         #region Tests de la méthode NbConsultationDepuisXSecondes
         // TODO Test E : HistoriqueTestNbConsultationDepuisXSecondesParamNegatifTest
         // Compléter la méthode pour tester le cas où la valeur du délai passé en paramètre est négative
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
         public void HistoriqueTestNbConsultationDepuisXSecondesParamNegatifTest()
         {
             // Arrange : Instancier un objet Historique
             // À compléter...
-          
+            Historique historique_test = new Historique();
             // Act : Appeler la méthode NbConsultationsDepuisXSecondes
             // À compléter...
-          
-            
+            historique_test.NbConsultationsDepuisXSecondes(-1);
+
             // Assert : Vérifier si la méthode lève une exception IndexOutOfRangeException
             // À compléter...
+            //[ExpectedException(typeof(IndexOutOfRangeException))]
         }
 
 
         // TODO Test F : HistoriqueTestNbConsultationDepuisXSecondesValideTest
         // Compléter la méthode pour tester le cas valide
+        [TestMethod()]
         public void HistoriqueTestNbConsultationDepuisXSecondesValideTest()
         {
             // Arrange : Instancier un objet Historique et y ajouter trois consultations d'une même chansonAAC
@@ -34,31 +39,41 @@ namespace BaladeurMultiFormatsTests
             // La deuxième consultation depuis 150 secondes (DateTime.AddSeconds(-150))
             // La troisième consultation depuis 300 secondes (DateTime.AddSeconds(-300))
             // À compléter...
-          
-            
-            
+            Historique historique_test = new Historique();
+            ChansonAAC chanson_Test = new ChansonAAC("Chansons\\Blame It On Me.aac");
+            DateTime date = DateTime.Now;
+            historique_test.Add(new Consultation(date.AddSeconds(-100),chanson_Test));
+            historique_test.Add(new Consultation(date.AddSeconds(-150), chanson_Test));
+            historique_test.Add(new Consultation(date.AddSeconds(-300), chanson_Test));
+
+
             // Act : Appeler la méthode NbConsultationsDepuisXSecondes pour calculer le nombre 
             // de chansons consultées depuis 200 secondes.
             // À compléter...
-           
+            int nb_Consultation = historique_test.NbConsultationsDepuisXSecondes(200);
+
             // Assert : Vérifier si la méthode retourne le bon nombre de consultations qui est 2 !
             // À compléter...
-            
+            Assert.AreEqual(2, nb_Consultation);
+
 
         }
         #endregion
- 
+
         #region Tests de la méthode NbConsultationsPourUneChanson
         // TODO Test G : HistoriqueTestNbConsultationsPourUneChansonParamNullTest
         // Compléter la méthode pour tester le cas où la chanson passée en paramètre est à null
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void HistoriqueTestNbConsultationsPourUneChansonParamNullTest()
         {
             // Arrange : Instancier un objet Historique
             // À compléter...
-         
-            
+            Historique historique_Test = new Historique();
+
             // Act : Appeler la méthode NbConsultationsDepuisXSecondes en lui passant la valeur null
             // À compléter...
+            historique_Test.NbConsultationsPourUneChanson(null);
            
 
             // Assert : Vérifier si la méthode lève une exception ArgumentNullException
@@ -67,6 +82,7 @@ namespace BaladeurMultiFormatsTests
 
         // TODO Test H : HistoriqueTestNbConsultationsPourUneChansonValideTest
         // Compléter la méthode pour tester le cas valide
+        [TestMethod()]
         public void HistoriqueTestNbConsultationsPourUneChansonValideTest()
         {
             // Arrange : Instancier un objet Historique et un objet ChansonAAC
@@ -76,20 +92,28 @@ namespace BaladeurMultiFormatsTests
             // La troisième consultation depuis 300 secondes (DateTime.AddSeconds(-300))
             // La quatrième consultation depuis 350 secondes (DateTime.AddSeconds(-350))
             // À compléter...
-          
+            Historique historique_test = new Historique();
+            ChansonAAC chanson_Test = new ChansonAAC("Chansons\\Blame It On Me.aac");
+            DateTime date = DateTime.Now;
+            historique_test.Add(new Consultation(date.AddSeconds(-100), chanson_Test));
+            historique_test.Add(new Consultation(date.AddSeconds(-150), chanson_Test));
+            historique_test.Add(new Consultation(date.AddSeconds(-300), chanson_Test));
+            historique_test.Add(new Consultation(date.AddSeconds(-350), chanson_Test));
+
 
             // Act : Appeler la méthode NbConsultationsDepuisXSecondes pour calculer le nombre 
             // de fois que la chansonAAC a été consultée.
             // À compléter...
+            int nb_Consultation = historique_test.NbConsultationsPourUneChanson(chanson_Test);
 
-           
             // Assert : Vérifier si la méthode retourne la bon nombre de consultations qui est 4
             // À compléter...
+            Assert.AreEqual(4, nb_Consultation);
 
-           
+
 
         }
-        
+
 
         #endregion
     }
